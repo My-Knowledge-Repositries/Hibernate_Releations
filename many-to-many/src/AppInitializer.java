e8b7a3ed51888e92aa97035171078235b318fe46
oshadaera68 <mindartlk010@gmail.com>
2023-04-20 22:55:32 +0530

    added new module

---
+++ many-to-many/src/AppInitializer.java
@@ -0,0 +1,31 @@
+import entity.Student;
+import org.hibernate.Session;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        /*  try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Laptop laptop1 = new Laptop();
+            Laptop laptop2 = new Laptop();
+            Student student = new Student();
+            laptop1.setBrand("Lenovo");
+            laptop1.setStudent(student);
+            laptop2.setBrand("Mac");
+            laptop2.setStudent(student);
+            student.setStudentName("Nimal");
+            student.setLaptop(Arrays.asList(laptop1,laptop2));
+            session.save(student);
+            transaction.commit();
+        }*/
+
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Student selectedStudent = session.get(Student.class, (long) 1);
+            System.out.println(selectedStudent.getStudentName());
+            System.out.println("================");
+            selectedStudent.getLaptop().stream().forEach(e-> System.out.println(e.getBrand()));
+            System.out.println("================");
+        }
+
+    }
+
+}
---
+++ many-to-many/src/entity/Student.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.List;
+
+@Entity(name = "student_table")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private long studentId;
+    private String studentName;
+
+    //--------------mapping----------------
+    @OneToMany(
+            mappedBy = "student",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Laptop> laptops;
+    //----------------mapping--------------
+
+    public Student() {
+    }
+
+    public Student(long studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public List<Laptop> getLaptop() {
+        return laptops;
+    }
+
+    public void setLaptop(List<Laptop> laptops) {
+        this.laptops = laptops;
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
